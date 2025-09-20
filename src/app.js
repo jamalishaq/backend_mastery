2d9e1d151eb2d465eaa2408449498a6af4618c48
jamaloflagos <toyinjamal@gmail.com>
2025-09-20 23:16:40 +0100

    security: add rate limit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import appRouter from "./routes/router.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+const limiter = rateLimit({windowMs: 5 * 60 * 1000, max: 5});
 
 app.use(express.json());
+app.use(limiter);
 
 app.use("/api/v1", appRouter);
 
